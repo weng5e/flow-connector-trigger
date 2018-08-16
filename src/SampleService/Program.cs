ad6935bf3836adff02694805faa366a92eccc320
Wuyi Weng <wuweng@microsoft.com>
2018-08-16 15:57:25 -0700

    - add AppInsights

--- src/SampleService/Program.cs
+++ src/SampleService/Program.cs
@@ -19,6 +19,7 @@ namespace SampleService
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
